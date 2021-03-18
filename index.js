acc07e1bde11fce48eeb70cbb51dbed33a2b6ec3
Andrew <atg27@uw.edu>
2021-03-18 07:34:54 -0700

    commit tree f43d40243d051805890627bff29fe2a77ef47bba

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+    return 'This one is on me!';
+  } else if (rideLength > 2000 && rideLength < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideLength > 2500) {
+    return 'No can do.';
+  }
+  }
+  
+
+function ternaryCheckCity(city){
+  const destination = (city === 'NYC') ?  "Ok, sounds good." : "No go."; 
+  return destination;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case tip = 'generous' :
+      return "Thank you so much.";
+      
+    case tip = 'not as generous' :
+      return "Thank you.";
+     
+    default: 
+      return "Bye.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
